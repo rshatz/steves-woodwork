477a571a43b159e22760217da5545e536853ac0e
rshatz <richardshatz@outlook.com>
2017-04-26 17:15:06 -0700

    commit

---
+++ includes/navbar.php
@@ -0,0 +1,37 @@
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Steve's Woodwork</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.php" class="caret-before">Home</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle caret-after" data-toggle="dropdown">
+                            <b class="caret"></b>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a tabindex="-1" href="index.php#available">Available boxes</a></li>
+                            <li><a tabindex="-1" href="index.php#sold">Sold boxes</a></li>
+                            <li><a tabindex="-1" href="index.php#custom">Custom boxes</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="custom.php">Custom Shop</a></li>
+                    <li><a href="engraving.php">Engraving</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
