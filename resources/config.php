02becdfce277d7e46329368ee473739c033140cf
rshatz <richardshatz@outlook.com>
2017-07-18 18:55:33 -0700

    added config.php

---
+++ resources/config.php
@@ -0,0 +1,23 @@
+<?php
+
+// turn on output buffering
+ob_start();
+
+// turn on sessions
+session_start();
+
+defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
+
+// directory constants
+defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
+defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
+
+// database constants
+defined("DB_HOST") ? null : define("DB_HOST", "localhost");
+defined("DB_USER") ? null : define("DB_USER", "root");
+defined("DB_PASSWORD") ? null : define("DB_PASSWORD", "");
+defined("DB_NAME") ? null : define("DB_NAME", "steveswoodwork");
+
+$connecton = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+require_once("functions.php");
+?>
